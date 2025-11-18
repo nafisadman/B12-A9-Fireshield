f7ace07ac021fb9d4f828d4e6607c532b9055a0b
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-11-18 21:10:05 +0600

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,46 +19,95 @@ const Navbar = () => {
       });
   };
   return (
-    <div className="flex justify-between items-center">
-      {/* Navbar Left Part */}
-      <div className="text-4xl font-bold">
-        🧸 Toy<span className="text-secondary">Topia</span>
-      </div>
+    <div>
+      {/* Old Navbar ------------------------------------------------------------------------------*/}
 
-      {/* Navbar Middle Part */}
-      <div className="nav flex gap-5 text-accent">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/profile">My Profile</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-      </div>
-      {/* Navbar Right Part */}
-      <div className="login-btn flex gap-5">
-        {user && (
-          <div
-            className="flex items-center"
-            onMouseEnter={() => setHover(true)}
-            onMouseLeave={() => setHover(false)}
-          >
-            {hover && <div className="flex">{user && user.email}</div>}
-            <img
-              className="w-12 rounded-full border"
-              src={`${user ? user.photoURL : userIcon}`}
-              alt=""
-            />
+      {/* New Navbar ------------------------------------------------------------------------------*/}
+
+      <div class="navbar bg-base-100 shadow-xl">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+
+            <ul
+              tabindex="0"
+              class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/profile">My Profile</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About Us</NavLink>
+              </li>
+            </ul>
           </div>
-        )}
-        {user ? (
-          <>
-            <button onClick={handleLogout} className="btn btn-primary px-10">
-              Logout
-            </button>
-            <ToastContainer />
-          </>
-        ) : (
-          <Link to="/auth/login" className="btn btn-primary px-10">
-            Login
-          </Link>
-        )}
+          <a class="btn btn-ghost text-xl">
+            🧸 Toy<span className="text-secondary">Topia</span>
+          </a>
+        </div>
+
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">My Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About Us</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div class="navbar-end">
+          <div className="login-btn flex gap-5">
+            {user && (
+              <div
+                className="flex items-center"
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+              >
+                {hover && <div className="flex">{user && user.email}</div>}
+                <img
+                  className="w-12 rounded-full border"
+                  src={`${user ? user.photoURL : userIcon}`}
+                  alt=""
+                />
+              </div>
+            )}
+            {user ? (
+              <>
+                <button onClick={handleLogout} class="btn btn-primary">
+                  Logout
+                </button>
+                <ToastContainer></ToastContainer>
+              </>
+            ) : (
+              <Link to="/auth/login" className="btn btn-primary">
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
